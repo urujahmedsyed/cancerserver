dac7d91cadeb2fb663ef769b8111a06bcbef2939
Uruj Ahmed Syed <118600268+urujahmedsyed@users.noreply.github.com>
2023-07-17 23:13:57 +0530

    modified:   models/usermodel.js

--- models/usermodel.js
+++ models/usermodel.js
@@ -2,9 +2,10 @@ const mongoose=require('mongoose');
 const User = new mongoose.Schema({
     name:{type:String, required:true},
     uname:{type:String, required:true, unique:true},
-    mobile:{type:String, required:true, unique:true},
+    mobile:{type:String, unique:true},
     password:{type:String, required:true},
-    hospital:{type:String, required:true}
+    hospital:{type:String, required:true},
+    email:{type:String, unique:true}
 }, {collection:'user-data-endo'}
 );
 const model=mongoose.model('UserData',User);
